164a9cd784147c79f5e78cb454d141d1894a0630
casperleerink <casperleerink@gmail.com>
2020-06-03 15:29:17 -0700

    starting scene a

--- sketch.js
+++ sketch.js
@@ -48,7 +48,7 @@ function setup() {
     for (let i= 0; i < textA.length; i++) {
         strokeColors.push(color(random(0, 255)));
     }
-    mgr.showScene(C, strokeColors);
+    mgr.showScene(A, strokeColors);
     // mgr.showScene(A);
 
 }
